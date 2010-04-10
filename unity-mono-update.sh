b299293b921581181264d26adab8310f933b121c
Logan Barnett <logustus@gmail.com>
2010-04-10 06:06:20 -0700

    Initial commit.

---
+++ unity-mono-update.sh
@@ -0,0 +1,5 @@
+cp /Library/Frameworks/Mono.framework/Versions/Current/lib/mono/2.0/gmcs.exe        /Applications/Unity/Unity.app/Contents/Frameworks/MonoCompiler.framework/
+cp /Library/Frameworks/Mono.framework/Versions/Current/lib/mono/2.0/System.Core.dll /Applications/Unity/Unity.app/Contents/Frameworks/MonoCompiler.framework/
+cp /Library/Frameworks/Mono.framework/Versions/Current/lib/mono/2.0/System.Core.dll /Applications/Unity/Unity.app/Contents/Frameworks/Mono.framework
+
+
